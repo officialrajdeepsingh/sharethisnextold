a55ab250b7d2e3e35727705fbe4f2c5bdccab77c
officialrajdeepsingh <radewala.2@gmail.com>
2023-01-25 00:00:09 +0530

    update the schedule

---
+++ netlify/functions/scheduled-function/scheduled-function.js
@@ -0,0 +1,26 @@
+const { schedule } = require('@netlify/functions')
+const axios = require('axios');
+
+// To learn about scheduled functions and supported cron extensions,
+// see: https://ntl.fyi/sched-func
+module.exports.handler = schedule('* * * * *', async () => {
+
+  (async () => {
+
+    try {
+
+      await axios.get("https://linkedin-share.netlify.app/api/linkedin").then(
+        () => {
+          return {
+            statusCode: 200,
+            body: JSON.stringify({ message: "Hello World" }),
+          }
+        }
+      )
+
+    } catch (error) {
+      console.log(error);
+    }
+
+  })();
+})
